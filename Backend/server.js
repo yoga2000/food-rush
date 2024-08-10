c3f2ec95a075081a529c5e96f1ca298df1e80a49
yoga2000 <yogaraj3102000@gmail.com>
2024-08-11 00:26:39 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -29,7 +29,7 @@ app.use(
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "token"],
     credentials: true,
   })
 );
